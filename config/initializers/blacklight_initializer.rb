b9a8adda5406b6eb880eab895843275b7d2659be
Paul Hoffman <paul@lola.flo.org>
2015-11-11 14:38:52 -0500

    Check in working code

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'e0b3c152bbd8364d9d20280ee5e124010663796660c52f41c3e201d3fb3a534213feb30d66c96d915e0790aea0c5d88e32638a26409286fc98aa8d988150d04c'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blacklight::Application.config.secret_key_base = 'd91c7d4e8e079e6759ee187a151623dc809cf79aa326dd0e253561838e597bbd360835371eced18cd7e94226892cb337346b2958c363b15bcbc37495a9c9d67b'
