1e83cb6f738fa992713d2db5d2b41a82d9b0b929
Paul Hoffman <paul@flo.org>
2016-02-19 16:44:22 -0500

    playing with reserves routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Blacklight::Application.routes.draw do
   root to: "catalog#index"
   blacklight_for :catalog
   devise_for :users
+
+  # The reserves controller
+  resources :reserves, only: [:index, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
